1625410b14039df5b6666b7dc9cc3fe907e1f8a4
doubiedev <thedoubiedev@gmail.com>
2025-03-05 11:38:37 +1100

    fix zenmode word wrapping

--- nvim/.config/nvim/lua/plugins/zenmode.lua
+++ nvim/.config/nvim/lua/plugins/zenmode.lua
@@ -10,9 +10,18 @@ return {
             },
         }
 
+        local zenActive = false
+
         vim.keymap.set("n", "<leader>zz", function()
             require("zen-mode").toggle()
-            vim.wo.wrap = false
+            zenActive = not zenActive
+
+            if zenActive then
+                vim.wo.wrap = true
+            else
+                vim.wo.wrap = false
+            end
+
             -- ColorMyPencils()
         end, {desc = "Toggle Zen Mode and customize appearance"})
     end
