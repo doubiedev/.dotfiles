577938197d127c0c6ff10f93dbc5b7846200a5d0
doubiedev <thedoubiedev@gmail.com>
2025-06-26 14:53:22 +1000

    disable automatic word wrap in zen mode

--- nvim/.config/nvim/lua/plugins/zenmode.lua
+++ nvim/.config/nvim/lua/plugins/zenmode.lua
@@ -10,19 +10,19 @@ return {
             },
         }
 
-        local zenActive = false
+        -- local zenActive = false
 
         vim.keymap.set("n", "<leader>zz", function()
             require("zen-mode").toggle()
-            zenActive = not zenActive
+            -- zenActive = not zenActive
+            --
+            -- if zenActive then
+            --     vim.wo.wrap = true
+            -- else
+            --     vim.wo.wrap = false
+            -- end
 
-            if zenActive then
-                vim.wo.wrap = true
-            else
-                vim.wo.wrap = false
-            end
-
-            -- ColorMyPencils()
-        end, {desc = "Toggle Zen Mode and customize appearance"})
+            ColorMyPencils()
+        end, { desc = "Toggle Zen Mode and customize appearance" })
     end
 }
