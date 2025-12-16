9f838d99569d516112b335c5e214148928f8383a
Nicholas Doublet <thedoubiedev@gmail.com>
2025-12-16 13:23:24 +1100

    update keybinds

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,7 +4,7 @@
 
 -- Editing Lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down", silent = true })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up", silent = true  })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up", silent = true })
 
 vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and maintain cursor position" })
 
@@ -16,7 +16,16 @@ vim.keymap.set("n", "N", "Nzzzv", { desc = "Search backward and centre view" })
 
 -- Yank & Paste
 vim.keymap.set("x", "<leader>p", "\"_dP", { desc = "Paste while keeping current text in register" })
+vim.keymap.set("n", "<leader>y", "\"+y", { desc = "Yank to clipboard" })
+vim.keymap.set("v", "<leader>y", "\"+y", { desc = "Yank selected text to clipboard" })
+vim.keymap.set("n", "<leader>Y", "\"+Y", { desc = "Yank current line to clipboard" })
+
+vim.keymap.set("n", "<leader>d", "\"_d", { desc = "Delete without yanking" })
+vim.keymap.set("v", "<leader>d", "\"_d", { desc = "Delete selection without yanking" })
 
 -- Escape remaps
 vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Exit insert mode" })
 vim.keymap.set("i", "<C-[>", "<Esc>", { desc = "Exit insert mode" })
+
+-- Other
+vim.keymap.set("n", "Q", "<nop>", { desc = "Disable Q key" })
