55bde8d16d7c5005e9199d34b9c82f306440eca9
Nicholas Doublet <thedoubiedev@gmail.com>
2026-01-08 15:59:16 +1100

    update lazyvim plugins

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "folke/snacks.nvim",
+        opts = {
+            picker = {
+                sources = {
+                    explorer = {
+                        hidden = true,
+                    },
+                    files = {
+                        hidden = true,
+                    },
+                    grep = {
+                        hidden = true,
+                    },
+                },
+            },
+        },
+    },
+}
