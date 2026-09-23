2a4398071b08ccea7c2525d9de4d3bc2c4aa049c
Nicholas Doublet <thedoubiedev@gmail.com>
2026-09-23 13:44:10 +1000

    ensure external monitors are on right side of laptop

--- hyprland/.config/hypr/monitors.lua
+++ hyprland/.config/hypr/monitors.lua
@@ -5,7 +5,7 @@ local omarchy_gdk_scale = 1
 local omarchy_monitor_scale = 1
 
 hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = omarchy_monitor_scale })
+hl.monitor({ output = "", mode = "preferred", position = "0x0", scale = omarchy_monitor_scale })
 
 -- Configure a specific monitor.
 -- hl.monitor({ output = "DP-2", mode = "2560x1440@144", position = "0x0", scale = 1 })
@@ -14,4 +14,4 @@ hl.monitor({ output = "", mode = "preferred", position = "auto", scale = omarchy
 -- hl.monitor({ output = "DP-2", mode = "preferred", position = "auto", scale = 1, transform = 1 })
 
 -- Personal monitor configuration
-hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@60", position = "auto", scale = omarchy_monitor_scale })
+hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@60", position = "1920x0", scale = omarchy_monitor_scale })
